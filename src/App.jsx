2915f6db85a98587caba80a36f1bc4900ac82b0c
Sankalp dikshit <dikshitsankalp97@gmail.com>
2023-12-29 18:12:31 +0530

    Todo app implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
 function App() {
 
   return (
     <center
       class="todo-container">
-      <h1>TODO App</h1>
+      <AppName />
+      <AddTodo />
+      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
+
+
       <div class="container text-center">
-        <div class="row">
-          <div class="col-6"><input type="text" placeholder="Enter Todo Here" />
-          </div>
-          <div class="col-4"><input type="date" /></div>
-          <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
-        </div>
-
-        <div class="row">
-          <div class="col-6"> Buy Milk</div>
-          <div class="col-4">04/10/2023</div>
-          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6"> Go to College</div>
-          <div class="col-4">04/10/2023</div>
-          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
       </div>
     </center>
   )
